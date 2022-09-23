5b572206e1d03c19477f259936cf68df9c7231cb
Charlie Jacomme <jacomme@crans.org>
2022-09-23 11:42:41 +0200

    Generate all models

--- lake-draft12/templates/generate-models.sh
+++ lake-draft12/templates/generate-models.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 jinja lake-edhoc.jinja.spthy -o ../lake-edhoc.spthy
 jinja lake-edhoc.jinja.spthy -D SignatureEvent t -D DDHEvent t -o ../lake-edhoc-Sig-DDH.spthy
+jinja lake-edhoc.jinja.spthy -D SignatureEvent t -o ../lake-edhoc-Sig.spthy
+jinja lake-edhoc.jinja.spthy -D DDHEvent t -o ../lake-edhoc-DDH.spthy
 jinja lake-edhoc.jinja.spthy -D HashEvent t -o ../lake-edhoc-Hash.spthy
 
 jinja lake-edhoc.jinja.spthy -D KEM t -o ../lake-edhoc-KEM.spthy
